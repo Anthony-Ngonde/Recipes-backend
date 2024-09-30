8bb860429becbd0bff3501a0cdff58320418abc5
Anthony Ngonde <anto.ngonde@gmail.com>
2024-09-30 12:59:16 +0300

    .env file added

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-from decouple import Config
+from decouple import config
 
 class Config:
      SECRET_KEY = config('SECRET_KEY')
+     SQLALCHEMY_TRACK_MODIFICATIONS = config('SQLALCHEMY_TRACK_MODIFICATIONS', default=False, cast=bool)
