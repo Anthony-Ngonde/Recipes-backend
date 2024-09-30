9a95bec56912192e5724285a7bfe992c4031360b
Anthony Ngonde <anto.ngonde@gmail.com>
2024-09-30 13:36:05 +0300

    Adding CRUD operations for recipes

--- main.py
+++ main.py
@@ -13,7 +13,15 @@ db.init_app(app)
 
 api = Api(app,doc='/docs')
 
-recipe_model=api.model()
+#model (serializer)
+recipe_model=api.model(
+    "Recipe",
+    {
+        "id":fields.Integer(),
+        "title":fields.String(),
+        "description":fields.String()
+    }
+)
 
 
 
@@ -21,6 +29,31 @@ recipe_model=api.model()
 class HelloResource(Resource):
     def get(self):
         return {"message": "Hello World"}
+
+
+@api.route('/recipes')
+class RecipeResource(Resource):
+    def get(self):
+        """Get all recipes"""
+        pass
+
+    def post(self):
+        """Create a new recipe"""
+        pass
+
+@api.route('/recipe/<int:id>')
+class RecipeResource(Resource):
+    def get(self, id):
+        """Get a recipe by id"""
+        pass
+
+    def put(self,id):
+        """Update a recipe by id"""
+        pass
+
+    def
+
+
 
 @app.shell_context_processor
 def make_shell_context():
